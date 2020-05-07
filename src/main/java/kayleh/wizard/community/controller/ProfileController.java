37f25bcab1a241fa53ff9101060160d97b5c7d56
Kayleh <924934908@qq.com>
2020-05-07 22:11:34 +0800

    12profile

---
+++ src/main/java/kayleh/wizard/community/controller/ProfileController.java
@@ -0,0 +1,73 @@
+package kayleh.wizard.community.controller;
+
+import kayleh.wizard.community.dto.PaginationDTO;
+import kayleh.wizard.community.mapper.UserMapper;
+import kayleh.wizard.community.model.User;
+import kayleh.wizard.community.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+ * @Author: Wizard
+ * @Date: 2020/5/7 12:11
+ */
+@Controller
+public class ProfileController {
+
+    @Autowired
+    private UserMapper userMapper;
+    @Autowired
+    private QuestionService questionService;
+
+
+    @GetMapping("/profile/{action}")
+    public String profile(@PathVariable("action") String action, Model model, HttpServletRequest request,
+                          @RequestParam(name = "page", defaultValue = "1") Integer page,
+                          @RequestParam(name = "size", defaultValue = "5") Integer size) {
+
+        User user = null;
+
+        Cookie[] cookies = request.getCookies();
+        //&&
+        if (cookies != null && cookies.length != 0) {
+            for (Cookie cookie : cookies) {
+                //字符串写在equal前面，防止空指针异常
+                if ((cookie.getName()).equals("token")) {
+                    String token = cookie.getValue();
+                    user = userMapper.findByToken(token);
+                    if (user != null) {
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
+                }
+            }
+        }
+
+        if (user==null){
+            return "redirect:/";
+        }
+
+        if ("questions".equals(action)) {
+            model.addAttribute("section", "questions");
+            model.addAttribute("sectionName", "我的提问");
+        } else if ("replies".equals(action)) {
+            model.addAttribute("section", "replies");
+            model.addAttribute("sectionName", "最新回复");
+        }
+
+        PaginationDTO paginationDTO = questionService.list(user.getId(), page, size);
+
+        model.addAttribute("pagination",paginationDTO);
+
+        return "profile";
+    }
+}
--- src/main/java/kayleh/wizard/community/service/QuestionService.java
+++ src/main/java/kayleh/wizard/community/service/QuestionService.java
@@ -28,19 +28,27 @@ public class QuestionService {
     public PaginationDTO list(Integer page, Integer size) {
 
         PaginationDTO paginationDTO = new PaginationDTO();
+        Integer totalPage;
+
         //所有的数量
         Integer totalCount = questionMapper.count();
-        paginationDTO.setPagination(totalCount,page,size);
 
+
+        if (totalCount % size == 0) {
+            totalPage = totalCount / size;
+        } else {
+            totalPage = totalCount / size + 1;
+        }
+
+
+        paginationDTO.setPagination(totalCount,page,size);
 
         if (page < 1) {
             page = 1;
         }
-        if (page > paginationDTO.getTotalPage()) {
-            page = paginationDTO.getTotalPage();
+        if (page > totalPage) {
+            page = totalPage;
         }
-
-
 
         // size*(page-1)
         Integer offset = size * (page - 1);
@@ -49,6 +57,52 @@ public class QuestionService {
 
 
         List<QuestionDTO> questionDTOList = new ArrayList<>();
+        for (Question question : questions) {
+            User user = userMapper.findById(question.getCreator());
+            QuestionDTO questionDTO = new QuestionDTO();
+            BeanUtils.copyProperties(question, questionDTO);
+            questionDTO.setUser(user);
+            questionDTOList.add(questionDTO);
+        }
+//        return questionDTOList;
+        paginationDTO.setQuestions(questionDTOList);
+
+        return paginationDTO;
+    }
+
+    public PaginationDTO list(Integer userId, Integer page, Integer size) {
+
+        PaginationDTO paginationDTO = new PaginationDTO();
+
+        Integer totalPage;
+
+        //所有的数量
+        Integer totalCount = questionMapper.countByUserId(userId);
+
+
+        if (totalCount % size == 0) {
+            totalPage = totalCount / size;
+        } else {
+            totalPage = totalCount / size + 1;
+        }
+
+        if (page < 1) {
+            page = 1;
+        }
+        if (page > totalPage) {
+            page = totalPage;
+        }
+
+        paginationDTO.setPagination(totalPage,page);
+
+
+        // size*(page-1)
+        Integer offset = size * (page - 1);
+
+        List<Question> questions = questionMapper.listByUserId(userId,offset,size);
+        List<QuestionDTO> questionDTOList = new ArrayList<>();
+
+
         for (Question question : questions) {
             User user = userMapper.findById(question.getCreator());
             QuestionDTO questionDTO = new QuestionDTO();
